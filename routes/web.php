71576781e9163d3fe312cb690eac80328a8a3974
xXNJEEBXx <xXNJEEBXx@GmAiL.CoM>
2022-07-22 20:02:29 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "test";
 });
 
 // Route::get('/register', [ApiController::class, 'register' ]);
-
-
